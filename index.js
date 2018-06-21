2cea79ca690ce6c94075cab647155f19ae796082
Maneesha <venigallamaneesha24@gmail.com>
2018-06-22 01:09:06 +0530

    testing oauth

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys.js');
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log('accessToken:', accessToken);
+	console.log('refreshToken:', refreshToken);
+	console.log('profile:', profile);
+}));
+
 
 const app = express();
 
-app.get('/',(req,res)=>{
-	res.send({hi:"batman & JOKER"});
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get('/', (req, res) => {
+	res.send({
+		hi: "batman & JOKER"
+	});
 });
 
 const PORT = process.env.PORT || 5000;
