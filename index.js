009c6c43b0570705275004077924dbdcbb236336
Maneesha <venigallamaneesha24@gmail.com>
2018-06-22 00:20:13 +0530

    changes committed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-	res.send({hi:"batman & joker"});
+	res.send({hi:"batman & JOKER"});
 });
 
 const PORT = process.env.PORT || 5000;
