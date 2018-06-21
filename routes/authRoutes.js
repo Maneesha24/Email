7e38a1b4e6f7a588668c987d246ded76086da9f5
Maneesha <venigallamaneesha24@gmail.com>
2018-06-22 01:58:01 +0530

    googleoauth tested

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,15 @@ module.exports = (app) => {
 		scope: ['profile', 'email']
 	}));
 
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
+	
+	app.get('/api/logout',(req,res)=>{
+		req.logout();
+		res.send(req.user);
+	});
+
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
 	app.get('/', (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,36 @@
 const passport = require('passport');
 const keys = require('../config/keys.js');
+const mongoose = require('mongoose');
+const user = mongoose.model('users');
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+	user.findById(id).then(user => {
+		done(null, user);
+	})
+});
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-	console.log('accessToken:', accessToken);
-	console.log('refreshToken:', refreshToken);
-	console.log('profile:', profile);
+	user.findOne({
+		googleID: profile.id
+	}).then(existingUser => {
+		if (existingUser) {
+			done(null, existingUser);
+		} else {
+			new user({
+				googleID: profile.id
+			}).save().then(user => done(null, user));
+		}
+	})
+
 }));
