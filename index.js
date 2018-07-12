fa831ca409906ea2d0ed80b16a69fbab9c0cdef2
Maneesha <venigallamaneesha24@gmail.com>
2018-07-12 14:50:43 +0530

    .

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys.js');
-require('.models/User');
+require('.models/user.js');
 require('./services/passport.js');
 require('./routes/billingRoutes.js');
 
