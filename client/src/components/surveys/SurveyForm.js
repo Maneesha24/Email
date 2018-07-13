4881f481ae6991a82566118ba59e865b95b53ed2
Maneesha <venigallamaneesha24@gmail.com>
2018-07-13 23:28:35 +0530

    state added

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import {reduxForm,Field} from 'redux-form';
 import SurveyField from './SurveyField.js';
 import {Link} from 'react-router-dom';
 import _ from 'lodash';
+import validateEmails from '../../utils/validateEmails.js';
 
 const FIELDS = [
 		{label : 'Survey Title', name : 'title'},
@@ -28,16 +29,31 @@ class SurveyForm extends React.Component {
 			<div>
 			<form onSubmit = {this.props.handleSubmit(values => console.log(values))}> 
 	         {this.renderFields()}
-<Link to ='/surveys' className = "red btn-flat white-text">Cancel</Link>
+            <Link to ='/surveys' className = "red btn-flat white-text">Cancel</Link>
 			  <button className = "teal btn-flat right white-text" type = "submit">Submit
               <i className = "material-icons right">done</i>
-</button>
+              </button>
 				</form>
 			</div>
 		);
 	}
 }
 
+function validate(values){
+	const errors = {};
+	
+	errors.emails = validateEmails(values.emails || '');
+	
+	_.each(FIELDS,({name})=>{
+		if(!values[name]){
+		   errors[name] = 'You must provide a value';
+		   }
+	});
+	
+	return errors;
+}
+
 export default reduxForm({
+	validate : validate,
 	form: 'surveyForm'
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import SurveyForm from './SurveyForm.js';
+import SurveyFormReview from './SurveyFormReview.js';
 
 class SurveyNew extends React.Component{
+	
+	state = {showFormReview : false};
+
+renderContent(){
+	if(this.state.showFormReview){
+		return <SurveyFormReview />
+	}
+		return <SurveyForm onSurveySubmit = {() => this.setState({showFormReview :true})}/>
+}
 	render(){
 		return(<div>
-			   <SurveyForm />
+			   {this.renderContent()}
 			   </div>);
 	}
 }
